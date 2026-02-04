e8a1b27f364007ad25f643edd9874172a595a7ae
XPPET <chris7greece@gmail.com>
2026-02-04 11:12:09 +0200

    Add ConverterLab2 project

---
+++ ConverterLab2/ConverterLab2/WebForm1.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ConverterLab2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            color: #0000FF;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1 class="auto-style1">Μετατροπή Συναλλάγματος</h1>
+            Δώστε νόμισμα προέλευσης:&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server" Width="28px"></asp:TextBox>
+&nbsp;&nbsp;
+            <asp:DropDownList ID="lstCurFrom" runat="server" Height="22px" Width="220px">
+            </asp:DropDownList>
+            <br />
+            <br />
+            Δώστε νόμισμα προορισμού:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="lstCurTo" runat="server" Height="24px" Width="220px">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Μετατροπή" />
+            <br />
+            <br />
+            Αποτέλεσμα: <asp:Label ID="Label1" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ConverterLab2/ConverterLab2/WebForm1.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace ConverterLab2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        lt.lb.www.ExchangeRates  myWS2 = new lt.lb.www.ExchangeRates();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                XmlNode node = myWS2.getListOfCurrencies();
+                XmlNodeList nodes = node.SelectNodes("//item");
+
+                foreach (XmlNode nd in nodes)
+                {
+                    lstCurFrom.Items.Add(nd["currency"].InnerText + " " +
+                         nd["description"].InnerText);
+
+                    lstCurTo.Items.Add(nd["currency"].InnerText + " " +
+                      nd["description"].InnerText);
+
+
+                    lstCurFrom.SelectedIndex = 26;
+                    lstCurTo.SelectedIndex = 27;
+
+                }
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string n1 =  Convert.ToString(lstCurFrom.SelectedItem).Substring(0,3);
+            string n2 = Convert.ToString(lstCurTo.SelectedItem).Substring(0, 3);
+
+            decimal f, rateFrom=0, rateTo = 0;
+
+            XmlNode node = myWS2.getExchangeRatesByDate("2014-12-31");
+
+            XmlNodeList nodes = node.SelectNodes("//item");
+
+            foreach(XmlNode nde in nodes)
+            {
+                if (nde["currency"].InnerText==n1)
+                   rateFrom = Convert.ToDecimal(nde["rate"].InnerText) 
+                        / Convert.ToInt32(nde["quantity"].InnerText);
+
+                if (nde["currency"].InnerText == n2)
+                    rateTo = Convert.ToDecimal(nde["rate"].InnerText)
+                         / Convert.ToInt32(nde["quantity"].InnerText);
+            }
+
+            f = Math.Round(Convert.ToDecimal(TextBox1.Text) * rateFrom / rateTo, 2);
+            Label1.Text = f.ToString();
+
+        }
+    }
+}
