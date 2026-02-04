0b915173d5c5545e51caaacdbfdf17186f2c8820
XPPET <chris7greece@gmail.com>
2026-02-04 11:20:35 +0200

    Add EURO2025Lab3 project

---
+++ EURO2025Lab3/EURO2025Lab3/WebForm1.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="EURO2025Lab3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            color: #0000CC;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1 class="auto-style1">EURO 2025</h1>
+            <asp:DropDownList ID="lstTeams" runat="server" AutoPostBack="True" Height="24px" OnSelectedIndexChanged="lstTeams_SelectedIndexChanged" Width="208px">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Label ID="lblTeamPlayers" runat="server" Visible="False"></asp:Label>
+            <br />
+            <br />
+            <asp:DropDownList ID="lstTeamPlayers" runat="server" AutoPostBack="True" Height="24px" Visible="False" Width="221px">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Label ID="lblTeamPlayersNS" runat="server" Visible="False"></asp:Label>
+            <br />
+            <br />
+            <asp:DropDownList ID="lstTeamPlayersNS" runat="server" AutoPostBack="True" Height="24px" Visible="False" Width="218px">
+            </asp:DropDownList>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ EURO2025Lab3/EURO2025Lab3/WebForm1.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EURO2025Lab3
+{ 
+        public partial class WebForm1 : System.Web.UI.Page
+    {
+        dev.dataflex.footballpool.Info fbWS =
+                       new dev.dataflex.footballpool.Info();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string[] TeamTable = fbWS.TeamNames();
+                lstTeams.Items.Add("-Επιλέξτε Εθνική Ομάδα-");
+
+                for (int i = 0; i < TeamTable.Length; i++)
+                    lstTeams.Items.Add(TeamTable[i]);
+
+            }
+        }
+
+        protected void lstTeams_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dev.dataflex.footballpool.tPlayer[]   Player  
+               = fbWS.TeamPlayers(lstTeams.Text, true);
+
+            lstTeamPlayers.Items.Clear();
+            for (int i = 0; i < Player.Length; i++)
+                lstTeamPlayers.Items.Add(Player[i].sName);
+
+
+            lblTeamPlayers.Text = "Οι παίκτες της ομάδας (" +
+               Convert.ToString(Player.Length) + "):";
+             lstTeamPlayers.Visible= lblTeamPlayers.Visible = true;
+
+            dev.dataflex.footballpool.tPlayer[] PlayerNS
+              = fbWS.TeamPlayers(lstTeams.Text, false);
+
+            lstTeamPlayersNS.Items.Clear();
+            for (int i = 0; i < PlayerNS.Length; i++)
+                lstTeamPlayersNS.Items.Add(PlayerNS[i].sName);
+
+
+            lblTeamPlayersNS.Text = "Οι μη επιλεγμένοι παίκτες της ομάδας (" +
+               Convert.ToString(PlayerNS.Length) + "):";
+            lstTeamPlayersNS.Visible = lblTeamPlayersNS.Visible = true;
+
+        }
+    }
+}
